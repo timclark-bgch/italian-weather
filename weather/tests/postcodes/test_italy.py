5f5ee49a9698b35c75559c5f1e16cebeeee93c76
Tim Clark <tim.clark@hivehome.com>
2017-03-30 11:37:15 +0100

    Test the postcode to location for Italy

--- weather/tests/postcodes/test_italy.py
+++ weather/tests/postcodes/test_italy.py
@@ -5,4 +5,8 @@ def test_location_for_postcode():
 	assert location_for_postcode('00118') == 'Roma, Lazio'
 	assert location_for_postcode('01100') == 'Viterbo, Lazio'
 	assert location_for_postcode('02100') == 'Rieti, Lazio'
+	assert location_for_postcode('03100') == 'Frosinone, Lazio'
+	assert location_for_postcode('04100') == 'Latina, Lazio'
+	assert location_for_postcode('05100') == 'Terni, Umbria'
+	assert location_for_postcode('06121') == 'Perugia, Umbria'
 
